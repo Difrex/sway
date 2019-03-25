c7ae36ee15645af1fdf445419ceadd920feb134b
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2019-03-25 14:29:52 +0300

    waybar colors change

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/swaylock -i "$(cat /home/difrex/.config/sway/.current_wallpaper)"
+/usr/bin/swaylock -i "$(cat /home/difrex/.config/sway/.current_wallpaper)" --indicator-radius 200 --font 'Source Code Pro' --ring-color '#002b36'
