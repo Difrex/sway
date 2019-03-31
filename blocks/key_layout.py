696c7c84e78c7c78bc85c5307db513403d50ad1c
Denis Zheleztsov <difrex.punk@gmail.com>
2019-03-31 09:53:42 +0300

    Fix keyboard layout block

--- blocks/key_layout.py
+++ blocks/key_layout.py
@@ -11,13 +11,8 @@ def get_inputs():
 def get_current_layout():
     inputs = get_inputs()
     for inp in inputs:
-        if inp.get("identifier", None) == "1118:1974:Microsoft_Comfort_Curve_Keyboard_3000":
-            if inp.get("xkb_active_layout_name", None) == "Russian":
-                return "Ru"
-    for inp in inputs:
-        if inp.get("identifier", None) == "1452:627:Apple_Inc._Apple_Internal_Keyboard_/_Trackpad":
-            if inp.get("xkb_active_layout_name", None) == "Russian":
-                return "Ru"
+        if inp.get("xkb_active_layout_name", None) == "Russian":
+            return "Ru"
     return "En"
 
 
