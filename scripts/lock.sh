159b2db9bf3de434c297a16f30c144c7205fff2e
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2020-02-21 09:43:14 +0300

    Change lock ring color

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/bin/swaylock -i "'$(cat /home/difrex/.config/sway/.current_wallpaper)'" --indicator-radius 200 --font 'Source Code Pro' --ring-color '#002b36'
+/usr/bin/swaylock -i "'$(cat /home/difrex/.config/sway/.current_wallpaper)'" --indicator-radius 190 --font 'Source Code Pro' --ring-color '#268bd2'
 # /usr/bin/swaylock -i "/tmp/simpletimed_crossfade.png" --indicator-radius 200 --font 'Source Code Pro' --ring-color '#002b36'
