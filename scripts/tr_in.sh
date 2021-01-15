6ba3f7a3cfe4d8707a40a00d3c967505b2882e83
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2021-01-15 11:28:21 +0300

    Switch to alacritty
    Also,
    
    * new wallpapper added
    * enable floating for a firefox window
    * fix kb layout script path in the waybar config
    * change monitor setup for lockdown remote working

--- scripts/tr_in.sh
+++ scripts/tr_in.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-export TR_IN_OPACITY="0.55"
+export TR_IN_OPACITY="0.75"
 PID=$(pgrep tr_in)
 
 if [[ -z $PID ]]; then
-    ${HOME}/.local/bin/tr_in
+    "${HOME}/.local/bin/tr_in"
 else
     killall tr_in
-    ${HOME}/.local/bin/tr_in
+    "${HOME}/.local/bin/tr_in"
 fi
