05e7e4efa4ab904ceb047e60f42cbb095bac0086
Denis Zheleztsov <d_zheleztsov@wargaming.net>
2021-01-15 12:32:02 +0300

    current_play script rewriten to use with a playerctl

--- blocks/current_play.sh
+++ blocks/current_play.sh
@@ -1,54 +1,13 @@
 #!/usr/bin/env bash
 
-echo $BLOCK_BUTTON > /tmp/block_button
+STATUS="$(playerctl status)"
 
-GRADIO=true
+if [[ "$STATUS" != "Playing" ]]; then
+    echo "$STATUS"
+else
+    artist="$(playerctl metadata xesam:albumArtist)"
+    title="$(playerctl metadata xesam:title)"
 
-# Pass the password in the block instance
-if [[ -n $BLOCK_INSTANCE ]]; then
-    password=("-h" "$BLOCK_INSTANCE@localhost")
+    echo "${artist} - ${title}"
+    echo "${artist} - ${title}"
 fi
-
-mpd_status=$(mpc $password status | tr '\n' ' ' | grep -Po '.*(?= \[playing\])|paused' | tr -d '\n')
-pctl_status=$(playerctl status)
-
-update_status() {
-    mpd_status=$(mpc $password status | tr '\n' ' ' | grep -Po '.*(?= \[playing\])|paused' | tr -d '\n')
-    pctl_status=$(playerctl status)
-}
-
-filter() {
-    update_status
-
-    echo -n '['
-    status=$mpd_status
-    if [[ -z $mpd_status ]] || [[ $mpd_status == "paused" ]]; then
-        if [[ $GRADIO ]]; then
-            if [[ $pctl_status == "Playing" ]]; then
-                status="radio"
-            fi
-        fi
-    fi
-    echo -n $status
-    echo -n ']'
-
-}
-
-play_pause() {
-    update_status
-
-    if [[ $GRADIO ]] && [[ $pctl_status == "Playing" ]]; then
-        playerctl pause
-    else
-        mpc $password toggle
-    fi
-
-    update_status
-}
-
-case $1 in
-    2) play_pause ;;
-    1) mpc $password prev   | filter ;;
-    3) mpc $password next   | filter ;;
-    *) mpc $password status | filter ;;
-esac
